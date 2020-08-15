92eaf91d91c34132d7bd1c9e35a6dc2612fce14b
Pablo Streiski <jose.pablo.com@hotmail.com>
2020-08-15 15:17:52 -0300

    Get all staged Calendar's tasks

--- src/controller/calendar.js
+++ src/controller/calendar.js
@@ -24,45 +24,10 @@ async function Calendar(page) {
     });
     
     return tasks;
-    
+
   } catch (error) {
     console.log("Evaluate error: \n", error);
   }
 }
 
 module.exports = Calendar;
-
-
-// function dale () {
-//     Promise.race([
-
-//     ])
-// }
-
-/*
-    let weeks = document.querySelectorAll(".calendarmonth > tbody > tr");
-    let results = { Semanas: [] };
-    console.log("entro na funcao");
-
-    weeks.forEach((index) => {
-      let days = index.querySelectorAll("td.day > div.d-none > div > ul");
-      let weeklyResults = [];
-
-      let iterator = 0;
-      days.forEach((element) => {
-        results.Semanas.push({
-          attr: element.querySelector("li > a").getAttribute("href"),
-          len: days.length,
-          it: iterator + 1,
-        });
-
-        iterator++;
-      });
-
-      //   results.Semanas.push({
-      //     tamanho: weeklyResults.length,
-      //     drele: [weeklyResults[0], weeklyResults[1], weeklyResults[2]],
-      //   });
-    });
-    
-    */
